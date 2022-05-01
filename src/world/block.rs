6b912ed02ce432e2245450dc0a725b96118b58fa
Firejoust <42525540+firejoust@users.noreply.github.com>
2022-05-01 14:54:12 +1000

    trailblazer: add from old repo

---
+++ src/world/block.rs
@@ -0,0 +1,4 @@
+pub struct Block {
+    id: u32,
+    shapes: Vec<[f64; 3]>
+}
---
+++ src/world/chunk.rs
@@ -0,0 +1,24 @@
+use crate::block::Block;
+
+struct BitArray {
+    data: Vec<u32>,
+    capacity: u32,
+    bits_per_value: u32,
+    value_mask: u32
+}
+
+struct ChunkSection {
+    data: BitArray,
+    palette: Vec<u32>,
+}
+
+struct ChunkColumn {
+    x: i32,
+    z: i32,
+    sections: Vec<ChunkSection>
+}
+
+struct ChunkGrid {
+    position: [i32; 3],
+    blocks: [[[Block; 16]; 16]; 16]
+}
